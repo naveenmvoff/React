345435160566983ea8b244aab46611f47f2c96e4
naveenmvoff <naveenmvoff@gmai.com>
2024-10-16 23:26:40 +0530

    Learning completed - components

---
+++ react-tutorial/src/components/car.js
@@ -0,0 +1,7 @@
+function Car(){
+    return(
+        <h2>Hi, I am a Car</h2>
+    );
+  }
+
+export default Car;
---
+++ react-tutorial/src/components/garage.js
@@ -0,0 +1,13 @@
+import Car from './car';
+
+function Garage() {
+    return(
+      <>
+      <h1>Who lives in the Garage?</h1>
+      <Car  />
+      </>
+      
+    )
+  }
+
+  export default Garage;
